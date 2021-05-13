7010fdb85104236c86aa9d74169b0a7c4fad8720
devarie <76944355+devarie@users.noreply.github.com>
2021-05-13 13:03:44 +0200

    commit but without images?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from './Contact';
+import ButtonAppBar from './ButtonAppBar';
+import Details from "./Details";
+import React from "react";
+import Home from './Home';
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+
+       
+           
+            <ButtonAppBar/>
+          
+          
+
+         
+
+        
+
+
+          
+          
+          
+      </div>
+   
   );
 }
-
-export default App;
